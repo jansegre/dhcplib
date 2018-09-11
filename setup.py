f13f91c9036da838fba0f272b55f4a9eae9c18fe
Jan Segre <jan@segre.in>
2018-09-11 11:19:34 -0300

    Bump version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dhcplib',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     description='Pure-Python, spec-compliant DHCP-packet-processing library',
     long_description=(
